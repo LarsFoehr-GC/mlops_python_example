7efe312e35818878fe1e2b3fb3c8ad065dc1f7cd
Lars Möller <larsmoller@Larss-MBP-2.fritz.box>
2022-07-21 17:26:35 +0200

    Added pre-commits

--- test_logging/test_logging.py
+++ test_logging/test_logging.py
@@ -1,14 +1,15 @@
 import logging
 
 logging.basicConfig(
-    format='%(asctime)s; %(levelname)s - %(name)s - %(message)s',
-    level=logging.DEBUG
-    )
+    format="%(asctime)s; %(levelname)s - %(name)s - %(message)s", level=logging.DEBUG
+)
 logger = logging.getLogger(__name__)
+
 
 def calc_plus(num1, num2):
     logger.info("Now the calculation starts. Yieah!")
-    return(num1 + num2)
+    return num1 + num2
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     calc_plus(1, 2)
